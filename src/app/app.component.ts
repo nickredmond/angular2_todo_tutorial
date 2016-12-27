81376a44da780f6c25e10ba3b2a2ecebec099600
Nick Redmond <nick.redmond@dealertrack.com>
2016-12-27 14:15:22 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+// Import class so we can register it as dependency injection token
+import { TodoDataService } from './todo-data.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+	newTodo: Todo = new Todo();
+
+	// Ask Angular DI system to inject the dependency
+	// associated with the dependency injection token `TodoDataService`
+	// and assign it to a property called `todoDataService`
+	constructor(private todoDataService: TodoDataService){
+	}
+
+	// Service is now available as this.todoDataService
+	toggleTodoComplete(todo){
+		this.todoDataService.toggleTodoComplete(todo);
+	}
+
+	addTodo(){
+		this.todoDataService.addTodo(this.newTodo);
+		this.newTodo = new Todo();
+	}
+
+	removeTodo(todo){
+		this.todoDataService.deleteTodoById(todo.id);
+	}
+
+	get todos(){
+		return this.todoDataService.getAllTodos();
+	}
+
 }
